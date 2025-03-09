95996a13ebea51e77a68a415298a5a8e918014f2
Aurian <aurian.marvilliers@gmail.com>
2025-03-09 20:39:04 +0100

    AreaPerimeter/

---
+++ AreaPerimeter/src/areaPerimeter.java
@@ -0,0 +1,11 @@
+public class areaPerimeter {
+    public static int areaOrPerimeter (int l, int w) {
+        // code away...
+        if (l!=w){ //rectangle --> perimetre
+            return 2*l+2*w;
+        }
+        else{
+            return l*l;
+        }
+    }
+}
