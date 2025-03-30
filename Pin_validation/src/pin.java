f127bb574a65154d61d57882a3d3aa53a230984a
Aurian <aurian.marvilliers@gmail.com>
2025-03-30 01:43:05 +0100

    Pin_validation/

---
+++ Pin_validation/src/pin.java
@@ -0,0 +1,16 @@
+public class pin {
+    public static boolean validatePin(String pin) {
+
+        if ((pin.length()!=6) && (pin.length() != 4 )){
+            return false;
+        }
+
+        for (int i = 0; i < pin.length(); i++){
+            if (!Character.isDigit(pin.charAt(i))){
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
