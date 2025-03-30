96176f2e9acd1f538f85938edabe793bb563980f
Aurian <aurian.marvilliers@gmail.com>
2025-03-30 01:43:39 +0100

    Int_to_String/

---
+++ Int_to_String/src/numberToString.java
@@ -0,0 +1,12 @@
+public class numberToString {
+    public static String numberToString(int num) {
+        // Return a string of the number here!
+        return Integer.toString(num);
+    }
+
+    public static void main (String[] args){
+        int nombre = 123;
+        var res = Integer.toString(nombre);
+        System.out.println(res);
+    }
+}
